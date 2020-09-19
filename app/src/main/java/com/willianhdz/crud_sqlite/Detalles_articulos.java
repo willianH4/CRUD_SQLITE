3606eb9cea20052f5e22f98e12728aa80ab5554a
Willian <williamhernandez450@gmail.com>
2020-09-19 07:13:10 -0600

    Cargando avances del proyecto

---
+++ app/src/main/java/com/willianhdz/crud_sqlite/Detalles_articulos.java
@@ -0,0 +1,14 @@
+package com.willianhdz.crud_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detalles_articulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_articulos);
+    }
+}
